eeacd5d0ce34ead3fff21eeae5bd17fc7d450d81
Scott Burns <scott@lentigo.net>
2025-02-07 14:19:06 -0500

    Set username and home directory from environment variables
    Use environment variables $USER and $HOME for home.username and
    home.homeDirectory respectfully. This requires passing the "--impure"
    option to "home-manager switch" the first time it's run after the
    change. This allows the use of this configuration on other machines
    without having to add per-host configurations to the repository. I'm
    okay with this for now.

--- home.nix
+++ home.nix
@@ -10,8 +10,8 @@ in
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "scott";
-  home.homeDirectory = "/Users/scott";
+  home.username = builtins.getEnv "USER";
+  home.homeDirectory = builtins.getEnv "HOME";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
